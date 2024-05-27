24765d305b793d9c71d202ea4926e53ac5a620e5
Brandon Kimathi <143943322+berry-ramon@users.noreply.github.com>
2024-05-27 22:43:11 +0300

    Created a responsive navigation bar

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Just for testing");
+const displayNavbar = document.querySelector(".navLinks");
+const show = document.querySelector(".menuOpen");
+const container = document.querySelector(".container");
+const hideNavbar = () => {
+  show.style.display = "block";
+  displayNavbar.style.transform = "translateX(400px)";
+  setTimeout(() => {
+    displayNavbar.style.display = "none";
+  }, 1000);
+};
+const showNavbar = () => {
+  show.style.display = "none";
+  setTimeout(() => {
+    displayNavbar.style.transform = "translateX(0px)";
+  }, 10);
+  displayNavbar.style.display = "block";
+};

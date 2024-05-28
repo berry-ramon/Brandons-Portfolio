ef9c612255039494ad5c009aa2f30ff4f054fad4
Brandon Kimathi <143943322+berry-ramon@users.noreply.github.com>
2024-05-28 13:30:09 +0300

    Improved the navigation bar and sidebar and also made them resposive

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 console.log("Just for testing");
-const displayNavbar = document.querySelector(".navLinks");
-const show = document.querySelector(".menuOpen");
-const container = document.querySelector(".container");
-const hideNavbar = () => {
-  show.style.display = "block";
-  displayNavbar.style.transform = "translateX(400px)";
-  setTimeout(() => {
-    displayNavbar.style.display = "none";
-  }, 1000);
+const displaySidebar = document.querySelector(".sidebar");
+const navbar = document.querySelector(".navbar");
+const viewSidebar = document.querySelector(".navbar li:nth-child(8)");
+
+const showSidebar = () => {
+  displaySidebar.style.transform = "translateX(0px)";
+  navbar.style.justifyContent = "space-between";
+  navbar.style.padding = "20px 10px";
+  viewSidebar.style.display = "none";
 };
-const showNavbar = () => {
-  show.style.display = "none";
-  setTimeout(() => {
-    displayNavbar.style.transform = "translateX(0px)";
-  }, 10);
-  displayNavbar.style.display = "block";
+
+const hideSidebar = () => {
+  displaySidebar.style.transform = "translateX(300px)";
+  navbar.style.justifyContent = "space-around";
+  navbar.style.padding = "20px";
+  viewSidebar.style.display = "block";
 };
+
+// Select all the links
+const navLinks = document.querySelectorAll(".navLinks li a");
+
+// Function to remove 'active' class from all links and add to the clicked one
+function handleNavLinkClick(event) {
+  navLinks.forEach((link) => link.classList.remove("active"));
+  event.currentTarget.classList.add("active");
+}
+
+// Add event listeners to all links
+navLinks.forEach((link) => {
+  link.addEventListener("click", handleNavLinkClick);
+});

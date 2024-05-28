381aada4cb97c704b6a86e8ba6756b700322f606
Brandon Kimathi <143943322+berry-ramon@users.noreply.github.com>
2024-05-28 19:22:03 +0300

    customized the image to blend well with small screens

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 console.log("Just for testing");
 const displaySidebar = document.querySelector(".sidebar");
 const navbar = document.querySelector(".navbar");
+const image1 = document.getElementById("image1");
+const iAm = document.querySelector(".left h2");
 const viewSidebar = document.querySelector(".navbar li:nth-child(8)");
 
 const showSidebar = () => {
@@ -8,6 +10,8 @@ const showSidebar = () => {
   navbar.style.padding = "20px 10px";
   viewSidebar.style.display = "none";
   displaySidebar.style.display = "block";
+  image1.style.display = "none";
+  iAm.style.fontSize = "15px";
   setTimeout(() => {
     displaySidebar.style.transform = "translateX(0px)";
   }, 1000);
@@ -17,6 +21,8 @@ const hideSidebar = () => {
   displaySidebar.style.transform = "translateX(300px)";
   navbar.style.justifyContent = "space-around";
   navbar.style.padding = "20px";
+  image1.style.display = "block";
+  iAm.style.fontSize = "20px";
   viewSidebar.style.display = "block";
   setTimeout(() => {
     displaySidebar.style.display = "none";

b0b67430fc75d7ba07efc400adfe7a5ae0bec715
Brandon Kimathi <143943322+berry-ramon@users.noreply.github.com>
2024-05-30 01:13:17 +0300

    Made the picture more official with afew bug fixes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("Just for testing");
 const displaySidebar = document.querySelector(".sidebar");
 const navbar = document.querySelector(".navbar");
 const iAm = document.querySelector(".left h2");
-const viewSidebar = document.querySelector(".navbar li:nth-child(8)");
+const viewSidebar = document.querySelector(".navbar li:nth-child(7)");
 
 const showSidebar = () => {
   navbar.style.justifyContent = "space-between";
@@ -26,3 +26,25 @@ const hideSidebar = () => {
     displaySidebar.style.display = "none";
   }, 1000);
 };
+
+// Select all the links
+const navLinks = document.querySelectorAll(".navbar .navLinks li");
+const navLinks2 = document.querySelectorAll(".navbar.sidebar li ");
+
+// Function to remove 'active' class from all links and add to the clicked one
+function handleNavLinkClick(event) {
+  navLinks.forEach((link) => link.classList.remove("active"));
+  event.currentTarget.classList.add("active");
+}
+const handleNavLinkClick2 = (event2) => {
+  navLinks.forEach((link2) => link2.classList.remove("active2"));
+  event2.currentTarget.classList.add("active2");
+};
+
+// Add event listeners to all links
+navLinks.forEach((link) => {
+  link.addEventListener("click", handleNavLinkClick);
+});
+navLinks2.forEach((link2) => {
+  link2.addEventListener("click", handleNavLinkClick2);
+});
